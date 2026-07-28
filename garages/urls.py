7407ad421801935899af6103e0c35dc646dd84e3
Ebikemeese <ebikemeese257@gmail.com>
2026-07-28 12:21:03 +0100

    fix pre deployment bugs

--- garages/urls.py
+++ garages/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'garages'
-
 urlpatterns = [
     path('garages/create/', views.create_garage, name='create_garage'),
     path('garages/<int:garage_id>/', views.garage_detail, name='garage_detail'),
--- verification/urls.py
+++ verification/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'verification'
 
 urlpatterns = [
     path('verification/toggle/<int:garage_id>/', views.toggle_verification, name='toggle_verification'),
